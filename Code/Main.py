8151ac79b446c8e3d0a911e3ebe3ca6ca18892da
lucasmrbarros <lucas.mr.barros@live.com>
2022-07-14 16:49:03 -0300

    Array feito

--- Code/Main.py
+++ Code/Main.py
@@ -1,4 +1,16 @@
 end = 0
 
-while end != 1:
-    print("hello")
+jogo = ["1","2","3","4","5","6","7","8","9"]
+
+for i in range(3):
+ print(jogo[i], end= "")
+
+print()
+
+for i in range(3, 6):
+ print(jogo[i], end= "")
+
+print()
+
+for i in range(6, 9):
+ print(jogo[i], end= "")
